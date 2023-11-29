21e431e39f127c3b4bf04f9169ccb9d3febe21da
milordplus <milordplus@mail.ru>
2023-11-29 19:23:30 +0300

    11

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
         <div className="footer-copyright">
             <div className="container">
                 © {new Date().getFullYear()} Copyright Text
-                <a className="grey-text text-lighten-4 right" href="#!">Repo</a>
+                <a className="grey-text text-lighten-4 right" href="https://github.com/milordplus/shop-project" rel="noreferrer" target="_blank">Repo</a>
             </div>
         </div>
     </footer>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
 function Header() {
     return <nav className="orange">
         <div className="nav-wrapper">
-            <a href="#" className="brand-logo">React Shop</a>
+            <a href="/" className="brand-logo">
+                React Shop
+            </a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
-                <li><a href="!#">Repo</a></li>
+                <li>
+                    <a href="https://github.com/milordplus/shop-project" target='_blank' rel="noreferrer">Repo</a>
+                </li>
             </ul>
         </div>
     </nav>
